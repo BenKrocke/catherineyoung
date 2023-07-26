5fa17ce1f313827eec3df641ade587979bfa8771
Ben Krocke <benkrocke@gmail.com>
2023-07-26 16:59:07 +1000

    Day two

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # catherineyoung
+
+Serve site for development purposes (unneccessary, but handy)
+`$ python3 -m http.server`
+
+Before development, run `$ npm i`, then, when developing, watch for styles with the tailwind CLI `$ npx tailwindcss -i ./stylesheet.css -o ./dist/output.css --watch` 
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'cyo-red': '#c2223d',
+        'cyo-red-light': '#D75860',
+        'cyo-blue': '#326B8A',
+        'cyo-blue-light': '#42A2D6',
+      }
+    },
+  },
+  plugins: [],
+}
+
