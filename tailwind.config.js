243a8ba9fedb600dbacc224970adaae07e69372e
Ben Krocke <benkrocke@pop-os.localdomain>
2023-08-14 12:46:06 +0930

    Add icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
         'cyo-red-light': '#D75860',
         'cyo-blue': '#326B8A',
         'cyo-blue-light': '#42A2D6',
+        'cyo-light-bg': '#F8F7F2'
       }
     },
   },
